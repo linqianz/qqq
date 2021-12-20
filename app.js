1a03b38403b7a318f22b1496f7030551cbe2edc5
linqianz <90674011+linqianz@users.noreply.github.com>
2021-12-19 21:02:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,55 +4,105 @@ App = {
 
   init: async function() {
     // Load pets.
+
+    let age = '';
+    let breed = '';
+    let pets = [];
     $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+      pets = data;
+      let ageSet = new Set();
+      let breedSet = new Set();
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+      for (let i = 0; i < pets.length; i ++) {
+        App.appendPet(pets[i]);
+        ageSet.add(pets[i].age);
+        breedSet.add(pets[i].breed);
+      }
 
-        petsRow.append(petTemplate.html());
+      for (let item of ageSet) {
+        $("#ageSelect").append("<option value='"+item+"'>"+item+"</option>");
+      }
+
+      for (let item of breedSet) {
+        $("#breedSelect").append("<option value='"+item+"'>"+item+"</option>");
+      }
+    });
+
+    $('#ageSelect').change(function(){
+      if (age !== $(this).children('option:selected').val()) {
+        age = $(this).children('option:selected').val();
+        App.filterPet(pets, age, breed);
+      }
+    });
+
+    $('#breedSelect').change(function(){
+      if (breed !== $(this).children('option:selected').val()) {
+        breed = $(this).children('option:selected').val();
+        App.filterPet(pets, age, breed);
       }
     });
 
     return await App.initWeb3();
   },
 
+  appendPet: function(pet) {
+    let petsRow = $('#petsRow');
+    let petTemplate = $('#petTemplate');
+    petTemplate.find('.panel-title').text(pet.name);
+    petTemplate.find('img').attr('src', pet.picture);
+    petTemplate.find('.pet-breed').text(pet.breed);
+    petTemplate.find('.pet-age').text(pet.age);
+    petTemplate.find('.pet-location').text(pet.location);
+    petTemplate.find('.btn-adopt').attr('data-id', pet.id);
+    petsRow.append(petTemplate.html());
+  },
+
+  filterPet: function(pets, age, breed) {
+    let petsRow = $('#petsRow');
+    petsRow.empty();
+    for (let i = 0; i < pets.length; i ++) {
+      if (!age && !breed) {
+        App.appendPet(pets[i]);
+      } else {
+        if (!age && breed) {
+          if (breed === (pets[i].breed + '')) {
+            App.appendPet(pets[i]);
+          }
+        } else if (age && !breed) {
+          if (age === (pets[i].age + '')) {
+            App.appendPet(pets[i]);
+          }
+        } else {
+          if (age === (pets[i].age + '') && breed === (pets[i].breed + '')) {
+            App.appendPet(pets[i]);
+          }
+        }
+      }
+    }
+  },
+
   initWeb3: async function() {
 
     // Modern dapp browsers...
-  if (window.ethereum) {
-    App.web3Provider = window.ethereum;
-    try {
-      // Request account access
-      await window.ethereum.enable();
-    } catch (error) {
-      // User denied account access...
-      console.error("User denied account access")
-    }
+if (window.ethereum) {
+  App.web3Provider = window.ethereum;
+  try {
+    // Request account access
+    await window.ethereum.enable();
+  } catch (error) {
+    // User denied account access...
+    console.error("User denied account access")
   }
-  // Legacy dapp browsers...
-  else if (window.web3) {
-    App.web3Provider = window.web3.currentProvider;
-  }
-  // If no injected web3 instance is detected, fall back to Ganache
-  else {
-    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-  }
-  web3 = new Web3(App.web3Provider);
-
-  // load default account data
-  web3.eth.getCoinbase(function(err, account) {
-    if (err == null){
-      App.account = account;
-      //console.log("coinbase account is", account);
-    }
-  });
+}
+// Legacy dapp browsers...
+else if (window.web3) {
+  App.web3Provider = window.web3.currentProvider;
+}
+// If no injected web3 instance is detected, fall back to Ganache
+else {
+  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+}
+web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
@@ -76,7 +126,6 @@ App = {
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
-
 
   markAdopted: function(adopters, account) {
     var adoptionInstance;
@@ -121,30 +170,6 @@ App = {
         console.log(err.message);
       });
     });
-  },
-
-  donateMeEther: function() {
-    App.contracts.Adoption.deployed().then(function(instance) {      
-      //send ether from the account to the contract
-      var amount = $('#donationAmount').val();
-      console.log(amount);
-      web3.eth.sendTransaction({
-        from: App.account,
-        to: App.contracts.Adoption.address,
-        value: amount
-      }, function(err, transactionHash) {
-        if(err) { console.log(err);}
-        else {
-          console.log(transactionHash + " success");
-        }
-      });
-      $(window).alert("Thanks for your donation");
-    });
-    
-    
-    
-
-
   }
 
 };
